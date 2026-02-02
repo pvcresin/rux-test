57041360951c312ae9e2c3bf2182acb87a7fd4ff
pvcresin <pvcresin0730@gmail.com>
2026-02-02 23:54:27 +0900

    Rux

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem "image_processing", "~> 1.2"
 
 gem "view_component"
 gem "typeprof"
+gem "rux-rails"
+gem "csv"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/components/name_component.rb
+++ app/components/name_component.rb
@@ -1,8 +1,16 @@
-# frozen_string_literal: true
-
 class NameComponent < ViewComponent::Base
   def initialize(first_name:, last_name:)
     @first_name = first_name
     @last_name = last_name
+  end
+
+  def call
+    Rux.tag("span") {
+      Rux.create_buffer.tap { |_rux_buf_|
+        _rux_buf_.append(@first_name)
+        _rux_buf_.safe_append(" ")
+        _rux_buf_.append(@last_name)
+      }.to_s
+    }
   end
 end
